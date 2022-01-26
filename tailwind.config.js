b97b5743bc4b342fe61f7d84c0357d071ee02156
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2022-01-27 03:17:18 +0530

    Added content to the home page

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      color0: "#182628",
+      color1: "#ffb414",
+      color2: "#8E8D8A",
+      color3: "#1f2d30",
+      color4: "#ffffff",
+      color5: "#EE4C7C",
+      color6: "#eeffff",
+      color7: "#89DDFF"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
